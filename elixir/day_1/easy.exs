b19b8176494f1493fee4418e3cabcda36c19c94f
Carlos Chacon <cchacon@growthaccelerationpartners.com>
2018-03-31 17:23:00 -0600

    Add Elixir Day 1's easy exercises

---
+++ elixir/day_1/easy.exs
@@ -0,0 +1,23 @@
+# Geometric tuples
+
+point = {10, 20}
+point |> inspect |> IO.puts
+
+
+# Calculate hypothenuse
+
+defmodule Triangle do
+  def hypotenuse({a, b}) do
+    :math.sqrt((a * a) + (b * b))
+  end
+end
+
+# Cast string to atom
+String.to_atom("super_cool")
+
+# Check is var is an atom
+is_atom("Cool") # false
+is_atom(:cool) # true
+
+
+
